c9b54f2fe49eb58d5233ee0884c93dfaefe53432
salvatorebramante <salvatore.bramante@yahoo.it>
2022-06-11 14:16:04 +0200

    aggiunte exception e migliorato lo scope

--- src/boundary/FormInserimento.java
+++ src/boundary/FormInserimento.java
@@ -103,23 +103,28 @@ public class FormInserimento extends JDialog {
 						&& !textField_Cittadino.getText().isEmpty() && !textField_Password.getText().isEmpty()) {
 				BCittadino cittadino = new BCittadino();
 				try {
-				
-				cittadino.richiediRimborso(
-						Integer.parseInt(textField_IDProgramma.getText()), textField_Cittadino.getText(),textField_Password.getText());
-;
-				//stampa messaggio di conferma al cittadino
-					JOptionPane.showConfirmDialog(contentPanel, "il rimborso è stato accreditato", "Conferma", JOptionPane.OK_CANCEL_OPTION);
-					
-				}catch(ProgrammaNonTrovato e1){
-					JOptionPane.showMessageDialog(contentPanel, e1.toString(),
-							"Errore", JOptionPane.ERROR_MESSAGE);
-				}catch(IscrizioneNonTrovata e2) {
-					JOptionPane.showMessageDialog(contentPanel, e2.toString(),
-							"Errore", JOptionPane.ERROR_MESSAGE);
+					cittadino.richiediRimborso(
+							Integer.parseInt(textField_IDProgramma.getText()), textField_Cittadino.getText(),textField_Password.getText());
+					//stampa messaggio di conferma al cittadino
+						JOptionPane.showConfirmDialog(contentPanel, "il rimborso è stato accreditato", "Conferma", JOptionPane.OK_CANCEL_OPTION);
+						
+					}catch(ProgrammaNonTrovato e1){
+						JOptionPane.showMessageDialog(contentPanel, e1.toString(),
+								"Errore", JOptionPane.ERROR_MESSAGE);
+					}catch(IscrizioneNonTrovata e2) {
+						JOptionPane.showMessageDialog(contentPanel, e2.toString(),
+								"Errore", JOptionPane.ERROR_MESSAGE);
+					}
+					catch(ProgrammaScaduto e3){
+						JOptionPane.showMessageDialog(contentPanel, e3.toString(),
+								"Errore", JOptionPane.ERROR_MESSAGE);
+					}
+					catch(PasswordErrata e4){
+						JOptionPane.showMessageDialog(contentPanel, e4.toString(),
+								"Errore", JOptionPane.ERROR_MESSAGE);
 					}
 				}
 			}
-			
 		});
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
